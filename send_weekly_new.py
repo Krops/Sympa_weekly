4f347ce79b4060acec584ec1e9f9c4c5c5b3a545
Andrii Kropyva <andrekropes@yandex.ru>
2015-12-10 13:27:35 +0200

    update new send

--- send_weekly_new.py
+++ send_weekly_new.py
@@ -28,6 +28,8 @@ while (1):
             weekly_list.append(row[0])
         except IndexError:
             sys.exit(1)
+# for test ------------------------------------
+weekly_list = ['rdktools-dev'] #!!!
 # print(weekly_list)
 
 # Create list of subscribed users
@@ -70,69 +72,79 @@ for user in user_list:
 #print(subscribed_list)
 cursor.close()
 mondbconn.close()
-print(subscribed_list)
+#print(subscribed_list)
 
 allowedGroups = {} #{'test-list':['rdk_employees', 'rdk_contractors', 'rdk_motorola']}
 #os.chdir('/home/sympa/list_data')
 #configdirs = [name for name in os.listdir(".") if os.path.isdir(name)]
 #print(configdirs)
-
+print(weekly_list)
 for dir in weekly_list:
     try:
         try:
             file=open('/mnt/sympa/list_data/{0}/config'.format(dir),'r')
         except:
             file=open('/home/sympa/list_data/{0}/config'.format(dir),'r')
+            print("{0} in home".format(dir))
     except IOError as e:
         print "I/O error({0}): {1}".format(e.errno, e.strerror)
-        
     text = file.read()
-    tes_str = re.search(r'custom_vars\nvalue (rdk_[a-z]+)(, rdk_[a-z]+)*',text).group(0)
-    tes_list = tes_str.replace('custom_vars\nvalue ','').split(', ')
-    allowedGroups[dir]=tes_list
+    try:
+        tes_str = re.search(r'custom_vars\nvalue ([a-z_-]+)(, [a-z_-]+)*',text).group(0)
+        tes_list = tes_str.replace('custom_vars\nvalue ','').split(', ')
+        allowedGroups[dir]=tes_list
+    except:
+        continue
     file.close()
-#print(allowedGroups)
+print(allowedGroups)
 
 # ldap initialize
-ldap_client = ldap.initialize('ldap://162.150.26.170:1389')
+ldap_client = ldap.initialize('ldap://:1389')
 ldap_base = 'ou=CCP,dc=ra,dc=ccp,dc=cable,dc=comcast,dc=com'
-
+#tesl = ldap_client.search_s(ldap_base, ldap.SCOPE_SUBTREE, '(cn={0})'.format('rdk_employee'))
+#print(tesl)
 # Send emails to users
 for key, value in subscribed_list.iteritems():
     weekly_msg = '<h3>New subscribtion lists for this week:</h3>'
     sub_list = '<html><head></head><body><h3>Your subscribtions:</h3>'
+    #print(key)
     email_info = ldap_client.search_s(ldap_base, ldap.SCOPE_SUBTREE, '(mail={0})'.format(key))
     try:
         user_uid = email_info[0][1]['uid'][0]
     except:
-        print(key)
-        print('non rdk user')
+        #print(key)
+        #print('non rdk user')
         user_uid = None
     for name in value:
         sub_list += '<div><label>{0}: </label><a href="https://rdklistmgr.ccp.xcal.tv/sympa/signoff/{0}">unsub</a></div>'.format(name)
     for name in weekly_list:
         if name in allowedGroups:
+            #print(allowedGroups[name])
             for group in allowedGroups[name]:
                 reviewers_result = ldap_client.search_s(ldap_base, ldap.SCOPE_SUBTREE, '(cn={0})'.format(group))
-                if reviewers_result:
+                if len(reviewers_result)>0:
                     if user_uid in reviewers_result[0][1].get('memberUid', []):
+                        print(user_uid)
                         if name in sub_list:
+                            print("yes")
                             weekly_msg += '<div><label>{0}: </label><b>new</b></div>'.format(name)
                         else:
+                            print("yes too")
                             weekly_msg += '<div><label>{0}: </label><a href="https://rdklistmgr.ccp.xcal.tv/sympa/subscribe/{0}">sub</a></div>'.format(name)
         else:
             if name in sub_list:
                 weekly_msg += '<div><label>{0}: </label><b>new</b></div>'.format(name)
             else:
                 weekly_msg += '<div><label>{0}: </label><a href="https://rdklistmgr.ccp.xcal.tv/sympa/subscribe/{0}">sub</a></div>'.format(name)
-    msg = MIMEText('{0}<br>{1}</body></html>'.format(sub_list,weekly_list), 'html')
+    msg = MIMEText('{0}<br>{1}</body></html>'.format(sub_list,weekly_msg), 'html')
     msg['Subject'] = 'RDK Mailing weekly subscription digest'
     msg['To'] = key
-    if 'ababich' in key:
+    print(key)
+    # for test!!!!
+    if 'raju_kakkerla' in key:
         print(msg.as_string())
-        p = Popen(["/usr/sbin/sendmail", "-t", "-oi"], stdin=PIPE)
-        p.communicate(msg.as_string())
-        print(msg.as_string())
+        #p = Popen(["/usr/sbin/sendmail", "-t", "-oi"], stdin=PIPE)
+        #p.communicate(msg.as_string())
+        #print(msg.as_string())
     #p = Popen(["/usr/sbin/sendmail", "-t", "-oi"], stdin=PIPE)
     #p.communicate(msg.as_string())
-
