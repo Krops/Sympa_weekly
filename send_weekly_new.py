7f1b54d24bd69fcb62942ff2282677725f37fd40
Andrii Kropyva <andrekropes@yandex.ru>
2015-12-10 17:04:13 +0200

    new final version

--- send_weekly_new.py
+++ send_weekly_new.py
@@ -28,9 +28,8 @@ while (1):
             weekly_list.append(row[0])
         except IndexError:
             sys.exit(1)
-# for testing!!!!!!!!!!!!!!!!!!!
-weekly_list = ['rdktools-dev']
-# print(weekly_list)
+# for testing
+weekly_list = ['arris_xg1v3-daily-changes', 'rdktools-dev', 'samsung_xg2v1-daily-changes']
 
 # Create list of subscribed users
 user_list = []
@@ -69,13 +68,10 @@ for user in user_list:
             except IndexError:
                 sys.exit(1)
     subscribed_list[user] = user_sub
-#print(subscribed_list)
 cursor.close()
 mondbconn.close()
-#print(subscribed_list)
 
-allowedGroups = {} #{'test-list':['rdk_employees', 'rdk_contractors', 'rdk_motorola']}
-print(weekly_list)
+allowedGroups = {}
 for dir in weekly_list:
     try:
         try:
@@ -86,20 +82,23 @@ for dir in weekly_list:
     except IOError as e:
         print "I/O error({0}): {1}".format(e.errno, e.strerror)
     text = file.read()
-    try:
-        tes_str = re.search(r'custom_vars\nvalue ([a-z_-]+)(, [a-z_-]+)*',text).group(0)
-        tes_list = tes_str.replace('custom_vars\nvalue ','').split(', ')
-        allowedGroups[dir]=tes_list
-    except:
+    res_groups = []
+    tes_str = re.findall(r'custom_vars\nvalue (.*)\n', text)
+    for lst in tes_str:
+        groups = [group.strip() for group in lst.split(',')]
+        res_groups += groups
+    if len(res_groups)>0:
+        allowedGroups[dir]=res_groups
+    else:
         continue
     file.close()
+print(weekly_list)
 print(allowedGroups)
 
 # ldap initialize
 ldap_client = ldap.initialize('ldap://:1389')
 ldap_base = 'ou=CCP,dc=ra,dc=ccp,dc=cable,dc=comcast,dc=com'
-#tesl = ldap_client.search_s(ldap_base, ldap.SCOPE_SUBTREE, '(cn={0})'.format('rdk_employee'))
-#print(tesl)
+
 # Send emails to users
 for key, value in subscribed_list.iteritems():
     weekly_msg = '<h3>New subscribtion lists for this week:</h3>'
@@ -109,19 +108,16 @@ for key, value in subscribed_list.iteritems():
     try:
         user_uid = email_info[0][1]['uid'][0]
     except:
-        #print(key)
-        #print('non rdk user')
+        #prin
         user_uid = None
     for name in value:
         sub_list += '<div><label>{0}: </label><a href="https://rdklistmgr.ccp.xcal.tv/sympa/signoff/{0}">unsub</a></div>'.format(name)
     for name in weekly_list:
         if name in allowedGroups:
-            #print(allowedGroups[name])
             for group in allowedGroups[name]:
                 ldap_info_group = ldap_client.search_s(ldap_base, ldap.SCOPE_SUBTREE, '(cn={0})'.format(group))
                 try:
                     if user_uid in ldap_info_group[0][1].get('memberUid', []):
-                        print(user_uid)
                         if name in sub_list:
                             print("fine")
                             weekly_msg += '<div><label>{0}: </label><b>new</b></div>'.format(name)
@@ -133,15 +129,11 @@ for key, value in subscribed_list.iteritems():
                 except:
                     continue
         else:
-            if name in sub_list:
-                weekly_msg += '<div><label>{0}: </label><b>new</b></div>'.format(name)
-            else:
-                weekly_msg += '<div><label>{0}: </label><a href="https://rdklistmgr.ccp.xcal.tv/sympa/subscribe/{0}">sub</a></div>'.format(name)
+            continue
     msg = MIMEText('{0}<br>{1}</body></html>'.format(sub_list,weekly_msg), 'html')
     msg['Subject'] = 'RDK Mailing weekly subscription digest'
     msg['To'] = key
     print(key)
-    #for testing!!!!!!!!!!!!!!!!!!!
     if 'raju_kakkerla' in key:
         print(msg.as_string())
         #p = Popen(["/usr/sbin/sendmail", "-t", "-oi"], stdin=PIPE)
@@ -149,5 +141,3 @@ for key, value in subscribed_list.iteritems():
         #print(msg.as_string())
     #p = Popen(["/usr/sbin/sendmail", "-t", "-oi"], stdin=PIPE)
     #p.communicate(msg.as_string())
-
-
